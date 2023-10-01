01b9e884b675e963f71a3eddefe0654554d0ed84
Mkrtich Kazanchian <mkogeo@gmail.com>
2023-10-01 15:17:09 +0400

    Add pagination.

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,17 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models.Repository;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
-      private readonly IStoreRepository repository;
+        private readonly IStoreRepository repository;
 
-      public HomeController(IStoreRepository repository)
+        public HomeController(IStoreRepository repository)
         {
             this.repository = repository;
         }
 
-      public IActionResult Index() => View(repository.Products);
+#pragma warning disable CA1051
+#pragma warning disable SA1201
+#pragma warning disable SA1401
+#pragma warning disable S1104
+        public int PageSize = 4;
+#pragma warning restore S1104
+#pragma warning restore SA1401 
+#pragma warning restore SA1201
+#pragma warning restore CA1051
+
+        public ViewResult Index(int productPage = 1)
+        {
+            return View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                               .OrderBy(p => p.ProductId)
+                               .Skip((productPage - 1) * PageSize)
+                               .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count(),
+                },
+            });
+        }
+
     }
 }
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -5,17 +5,24 @@ using SportsStore.Models.Repository;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-  
+
 builder.Services.AddDbContext<StoreDbContext>(opts =>
 {
-     opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
- });
-
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+
 var app = builder.Build();
 
 app.UseStaticFiles();
 
+app.MapControllerRoute(
+    name: "pagination",
+    pattern: "Products/Page{productPage:int}",
+    defaults: new { Controller = "Home", action = "Index", productPage = 1 });
+
 app.MapDefaultControllerRoute();
+
 SeedData.EnsurePopulated(app);
+
 app.Run();
