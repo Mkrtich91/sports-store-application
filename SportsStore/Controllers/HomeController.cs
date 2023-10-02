86feced3ab4b82c0decbb1f00da3f58da4ee68c0
MKRTICH KAZANCHIAN <mkogeo@gmail.com>
2023-10-02 21:01:36 +0000

    Revert "Revert "Creating the Identity Database.""
    This reverts commit 3c760be8356329b91ca756ba10d855b8c5bdd950

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -41,5 +41,11 @@ namespace SportsStore.Controllers
  
          CurrentCategory = category,
      });
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+
     }
 }
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SportsStore.Models;
 using SportsStore.Models.Repository;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddDbContext<StoreDbContext>(opts =>
 {
     opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
 });
+
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 builder.Services.AddScoped<IOrderRepository, EFOrderRepository>();
 builder.Services.AddDistributedMemoryCache();
@@ -17,45 +19,64 @@ builder.Services.AddSession();
 builder.Services.AddScoped<Cart>(SessionCart.GetCart);
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-var app = builder.Build();
+builder.Services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(builder.Configuration["ConnectionStrings:IdentityConnection"]));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
 
+var app = builder.Build();
+  
+if (app.Environment.IsProduction())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStatusCodePages();
 app.UseStaticFiles();
 app.UseSession();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllerRoute(
     name: "pagination",
     pattern: "Products/Page{productPage:int}",
     defaults: new { Controller = "Home", action = "Index", productPage = 1 });
 
 app.MapControllerRoute(
-     name: "categoryPage",
-     pattern: "{category}/Page{productPage:int}",
-     defaults: new { Controller = "Home", action = "Index" });
-  
+    name: "categoryPage",
+    pattern: "{category}/Page{productPage:int}",
+    defaults: new { Controller = "Home", action = "Index" });
+
 app.MapControllerRoute(
     name: "category",
     pattern: "Products/{category}",
     defaults: new { Controller = "Home", action = "Index", productPage = 1 });
+
 app.MapControllerRoute(
-      name: "shoppingCart",
-      pattern: "Cart",
-      defaults: new { Controller = "Cart", action = "Index" });
+    name: "shoppingCart",
+    pattern: "Cart",
+    defaults: new { Controller = "Cart", action = "Index" });
 
 app.MapControllerRoute(
     name: "default",
     pattern: "/",
     defaults: new { Controller = "Home", action = "Index" });
-app.MapControllerRoute(
-      "checkout",
-      "Checkout",
-      new { Controller = "Order", action = "Checkout" });
 
 app.MapControllerRoute(
-      "remove",
-      "Remove",
-      new { Controller = "Cart", action = "Remove" });
+    name: "checkout",
+    pattern: "Checkout",
+    defaults: new { Controller = "Order", action = "Checkout" });
 
-app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+    name: "remove",
+    pattern: "Remove",
+    defaults: new { Controller = "Cart", action = "Remove" });
+  
+app.MapControllerRoute(
+      "error",
+      "Error",
+      new { Controller = "Home", action = "Error" });
 
 SeedData.EnsurePopulated(app);
+IdentitySeedData.EnsurePopulated(app);
 
 app.Run();
